be5f2b53564c8377f0e0a2626f5bde44ee94c3e2
Tu Nombre <investingmtz@gmail.com>
2020-11-15 18:25:11 -0600

    Agregando impresion de datos

--- ejemplo.py
+++ ejemplo.py
@@ -3,12 +3,15 @@ class persona():
 		self.edad = edad
 		self.estatura = estatura
 
+	def printData(self):
+		print(f'Edad : {edad} Estatura: {estatura}')
+
 def main():
 	edad = int(input('Ingresa tu edad: '))
 	estatura = float(input('Ingresa tu estatura: '))
 	nombre = input('DIme tu nombre: ')
 
 	nombre = persona(edad,estatura)
-	print(nombre.edad, nombre.estatura)
+	nombre.printData
 
 main()
