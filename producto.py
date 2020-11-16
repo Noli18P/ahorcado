98c47b516de78c098a546dbe82ad9353022060f8
Tu Nombre <investingmtz@gmail.com>
2020-11-15 18:59:41 -0600

    Agregando setter y getter

--- producto.py
+++ producto.py
@@ -9,6 +9,33 @@ class producto:
 		self.__nombre = nombre
 		self.__precio = precio
 
+	#Getter
+	@property
+	def codigo(self):
+		return self.__codigo
+
+	@property
+	def __nombre(self):
+		return self.___nombre
+	
+	@property
+	def __precio(self):
+		return self.___precio
+	
+	#Setter
+	@codigo.setter
+	def codigo(self, valor):
+		self.__codigo = valor
+
+	@nombre.setter
+	def nombre(self,valor):
+		self.__nombre = valor
+
+	@precio.setter
+	def precio(self,valor):
+		self.__precio = valor
+
+	#Metodo
 	def calcular_total(self, unidades):
 		total = precio * unidades
 
