a995ce3528c186677abf0fa7908d010911a5dc02
Tu Nombre <investingmtz@gmail.com>
2020-11-15 18:51:51 -0600

    Terminando enunciado

--- producto.py
+++ producto.py
@@ -1,6 +1,7 @@
 #Crear una clase llamada producto con los siguientes atributos
 # codigo, nombre, precio
-#
+# Crear su constructor, getter y setter y un método llamado calcular_total al que le pasaremos unas unidades 
+#y debe calcular el precio final
 
 class producto:
 	def __init__(self, codigo, nombre, precio):
