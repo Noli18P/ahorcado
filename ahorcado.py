87b6a9935ccfb2308181cc9fa54712456b92fc03
Tu Nombre <investingmtz@gmail.com>
2020-11-16 15:44:46 -0600

    Proyecto finalizado

--- ahorcado.py
+++ ahorcado.py
@@ -65,20 +65,48 @@ def generar_palabra_secreta():
         'huron', 'tortuga', 'cucaracha', 'cuervo', 'gusano', 'colibri', 'serpiente', 'nutria', 'pato'
     ]
 
-    palabra_secreta = random.choices(palabras)
+    n = random.randint(0, len(palabras))
+    palabra_secreta = palabras[n]
 
     return palabra_secreta
 
-def mostrar_tablero(intentos):
-    if intentos == 0:
-        print('     A H O R C A D O')
-    print(imagenes_ahorcado[intentos])
 
 def main():
     palabra_generada = generar_palabra_secreta()
-    list(palabra_generada)
+    palabra = ''
+    vidas = 5
+    intentos = 0
 
-    print(palabra_generada)
+    while vidas >= 0:
+        error = 0
+        for letra in palabra_generada:
+            if letra in palabra:
+                print(letra,end=(' '))
+            else:
+                print('_',end=(' ')) 
+                error += 1
+        if error == 0:
+            print('\nFelicidades la palabra ers: ', palabra_generada)
+            break
+        
+        if vidas == 0:
+            break
+        else:
+            print(imagenes_ahorcado[intentos])
+            letra_usuario = input('\nIntroduce una letra: ').lower()
+            palabra += letra_usuario
+            
+    
+        if letra_usuario not in palabra_generada:
+            vidas -= 1
+            intentos += 1
+            print('\nError! Intentalo de nuevo')
+    
+        
+        if vidas == 0:
+            print(imagenes_ahorcado[6])
+            print("\nPerdiste! La palabra era: ", palabra_generada)
+    
 
 if __name__ == '__main__':
     main()
