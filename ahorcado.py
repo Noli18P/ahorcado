bfe5a1259c3c6e389c562b10ac6f432c8f968293
Tu Nombre <investingmtz@gmail.com>
2020-11-15 22:53:33 -0600

    Corrigiendo estructura de código

--- ahorcado.py
+++ ahorcado.py
@@ -1,19 +1,7 @@
 #GENERAR UNA PALABRA SECRETA
 import random
 
-def generar_palabra_secreta():
-    palabras = [
-        'mono', 'perro', 'gato', 'loro', 'gorila', 'zorro', 'aguila', 'hamster', 'tigre', 'leon', 'raton',
-        'huron', 'tortuga', 'cucaracha', 'cuervo', 'gusano', 'colibri', 'serpiente', 'nutria', 'pato'
-    ]
-
-    numero_random = random.randint(0, len(palabras) - 1)
-    palabra = palabras[numero_random]
-    
-    return palabra
-
-def mostrar_tablero():
-    tablero = ["""
+imagenes_ahorcado = ["""
     A H O R C A D O
       +---+
       |   |
@@ -70,15 +58,24 @@ def mostrar_tablero():
      / \  |
           |
     =========
-    """]    
-
-    return tablero[0]
-
-def espacios_vacios(palabra_secreta):
-    for i in palabra_secreta:
-        print('_', end=(' '))
+    """]
 
 
+def generar_palabra_secreta():
+    palabras = [
+        'mono', 'perro', 'gato', 'loro', 'gorila', 'zorro', 'aguila', 'hamster', 'tigre', 'leon', 'raton',
+        'huron', 'tortuga', 'cucaracha', 'cuervo', 'gusano', 'colibri', 'serpiente', 'nutria', 'pato'
+    ]
 
-print(mostrar_tablero())
-espacios_vacios(generar_palabra_secreta())
+    numero_random = random.randint(0, len(palabras) - 1)
+    palabra = palabras[numero_random]
+    
+    return list(palabra)
+
+
+def mostrar_tablero(tablero,palabra_secreta):
+    list(palabra_secreta)
+    print(tablero)
+    print(palabra_secreta)
+
+mostrar_tablero(imagenes_ahorcado[0],generar_palabra_secreta())
