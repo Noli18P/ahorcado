83c032b257675c8761ba413afeac646f0cd91a39
Tu Nombre <investingmtz@gmail.com>
2020-11-15 18:50:02 -0600

    Agregando clase

---
+++ producto.py
@@ -0,0 +1,9 @@
+#Crear una clase llamada producto con los siguientes atributos
+# codigo, nombre, precio
+#
+
+class producto:
+	def __init__(self, codigo, nombre, precio):
+		self.codigo = codigo
+		self.nombre = nombre
+		self.precio = precio
