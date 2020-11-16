9f7433180dee74e173d3fd91956e0839fa19cf0c
Tu Nombre <investingmtz@gmail.com>
2020-11-15 22:01:34 -0600

    Agregue animales y arregle bug de numero fuera de rango

--- ahorcado.py
+++ ahorcado.py
@@ -3,9 +3,14 @@ import random
 
 def generar_palabra_secreta():
     palabras = [
-        'mono', 'perro', 'gato', 'loro', 'gorila', 'zorro', 'aguila', 'hamster', 'tigre', 'leon'
+        'mono', 'perro', 'gato', 'loro', 'gorila', 'zorro', 'aguila', 'hamster', 'tigre', 'leon', 'raton',
+        'huron', 'tortuga', 'cucaracha', 'cuervo', 'gusano', 'colibri', 'serpiente', 'nutria', 'pato'
     ]
 
-    numero_random = random.randint(palabras.lenght)
+    numero_random = random.randint(0, len(palabras) - 1)
     palabra = palabras[numero_random]
+    
     return palabra
+
+palabra_random = generar_palabra_secreta()
+print(palabra_random)
