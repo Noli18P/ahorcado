427701182ee378116354562ed889510c5385e1d1
Tu Nombre <investingmtz@gmail.com>
2020-11-15 18:22:29 -0600

    Agregando soluciones

--- ejemplo.py
+++ ejemplo.py
@@ -4,9 +4,9 @@ class persona():
 		self.estatura = estatura
 
 def main():
-	edad = int(input('Ingresa tu edad'))
-	estatura = float(input('Ingresa tu estatura'))
-	nombre = input('DIme tu edad: ')
+	edad = int(input('Ingresa tu edad: '))
+	estatura = float(input('Ingresa tu estatura: '))
+	nombre = input('DIme tu nombre: ')
 
 	nombre = persona(edad,estatura)
 	print(nombre.edad, nombre.estatura)
